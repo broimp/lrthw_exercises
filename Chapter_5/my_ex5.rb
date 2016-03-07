fdb8a6f37931ef41a19da5f618f9be06b5d9e12c
Brooks Imperial <brooks@forethought.net>
2016-03-07 13:45:10 -0700

    Fixed Rubocop warnings for long lines [#113796407]

--- Chapter_5/my_ex5.rb
+++ Chapter_5/my_ex5.rb
@@ -14,11 +14,16 @@ kilos = weight * 0.453592 # 1 lb = 0.453592 kg
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
 puts "He's #{weight} pounds heavy."
-puts "He's #{format('%.2f', kilos)} kilos heavy."  # round answer to 2 significant digits
-puts "He's #{format('%.1f', centimeters)} centimeters tall."  # round answer to 1 significant digit
+
+# round answer to 2 significant digits
+puts "He's #{format('%.2f', kilos)} kilos heavy."
+
+# round answer to 1 significant digit
+puts "He's #{format('%.1f', centimeters)} centimeters tall."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # This line is tricky, try to get it exactly right
-puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+print "If I add #{age}, #{height}, and "
+puts "#{weight} I get #{age + height + weight}."
